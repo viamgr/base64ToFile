c63d2bca9ed65859799de9ac0489007a77b23d27
awc_moradi <vahid.moradi@artanwebcreator.com>
2016-10-22 11:49:25 +0330

    Add isMatchToBase64 method and remove some unused logs and prettify.

--- index.js
+++ index.js
@@ -1,62 +1,89 @@
+var mkdirp = require('mkdirp');
+
+exports.isMatchToBase64 = function isMatchToBase64(dataString) {
+    return dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+};
+
 function decodeBase64Image(dataString) {
-  var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-  var response = {};
+    var matches = exports.isMatchToBase64(dataString);
+    if (matches && matches.length) {
+        var response = {};
+        if (matches.length !== 3) {
+            return new Error('Invalid input string');
+        }
+        response.type = matches[1];
+        response.data = new Buffer(matches[2], 'base64');
 
-  if (matches.length !== 3) {
-    return new Error('Invalid input string');
-  }
-  response.type = matches[1];
-  response.data = new Buffer(matches[2], 'base64');
+        return response;
+    }
+    else {
+        throw 'Input string is not a base64.';
+    }
 
-  return response;
 }
 
 
-exports.convert = function (base64String, uploadLocation, acceptFileTypes,callback) {
-  console.log("This is a message from the demo package");
+exports.convert = function (base64String, uploadLocation, acceptFileTypes, callback) {
 
+    var fs = require('fs');
 
 // Regular expression for image type:
 // This regular image extracts the "jpeg" from "image/jpeg"
-  var imageTypeRegularExpression = /\/(.*?)$/;
+    var imageTypeRegularExpression = /\/(.*?)$/;
 
 // Generate random string
-  var crypto = require('crypto');
-  var seed = crypto.randomBytes(10);
-  var uniqueSHA1String = crypto
-    .createHash('sha1')
-    .update(seed)
-    .digest('hex');
+    var crypto = require('crypto');
+    var seed = crypto.randomBytes(10);
+    var uniqueSHA1String = crypto
+        .createHash('sha1')
+        .update(seed)
+        .digest('hex');
 
 
-  var imageBuffer = decodeBase64Image(base64String);
+    var imageBuffer = decodeBase64Image(base64String);
 
-  var uniqueRandomImageName = 'image-' + uniqueSHA1String;
-  var imageTypeDetected = imageBuffer
-    .type
-    .match(imageTypeRegularExpression);
+    var uniqueRandomImageName = 'image-' + uniqueSHA1String;
+    var imageTypeDetected = imageBuffer
+        .type
+        .match(imageTypeRegularExpression);
 
-  var extension = imageTypeDetected[1];
+    var extension = imageTypeDetected[1];
 
-  if (acceptFileTypes.indexOf(extension) < 0) {
-    throw new Error('Invalid file type');
-  }
+    if (acceptFileTypes.indexOf(extension) < 0) {
+        throw new Error('Invalid file type');
+    }
 
-  var userUploadedImagePath = uploadLocation +
-    uniqueRandomImageName +
-    '.' +
-    extension;
+    var writeFile = function () {
+        var userUploadedImagePath = uploadLocation +
+            uniqueRandomImageName + '.' +
+            extension;
 
-// Save decoded binary image to disk
-  try {
-    require('fs').writeFile(userUploadedImagePath, imageBuffer.data,
-      function () {
-        callback(userUploadedImagePath);
-      });
-  }
-  catch (error) {
-    throw new Error(error);
-  }
+        try {
+            fs.writeFile(userUploadedImagePath, imageBuffer.data,
+                function () {
+                    callback(userUploadedImagePath);
+                });
+        }
+        catch (error) {
+            throw new Error(error);
+        }
+    };
+
+    if (!fs.existsSync(uploadLocation)) {
+        console.log("index-50", uploadLocation);
 
 
-}
+        mkdirp(uploadLocation, function (err) {
+            if (err) {
+                throw error;
+            }
+            writeFile();
+        });
+
+    }
+    else {
+        writeFile();
+    }
+
+
+};
